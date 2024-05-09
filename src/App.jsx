0de784eebf6b109aaa28510289f960b73efb7bd7
dimnov <dimnov1512@gmail.com>
2024-05-10 00:53:56 +0300

    Add basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
